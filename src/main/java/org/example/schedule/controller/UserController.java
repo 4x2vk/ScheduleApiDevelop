6d3a9305bfdfa9a2f1bd6feeb5604351b4790be1
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-08 12:05:35 +0900

    [feat] implement findUsers

--- src/main/java/org/example/schedule/controller/UserController.java
+++ src/main/java/org/example/schedule/controller/UserController.java
@@ -1,13 +1,14 @@
 package org.example.schedule.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.schedule.dto.UserGetAllResponse;
 import org.example.schedule.dto.UserSaveRequest;
 import org.example.schedule.dto.UserSaveResponse;
 import org.example.schedule.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +19,10 @@ public class UserController {
     @PostMapping("/users")
     public ResponseEntity<UserSaveResponse> saveUser(@RequestBody UserSaveRequest userSaveRequest) {
         return ResponseEntity.ok(userService.saveUser(userSaveRequest));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserGetAllResponse>> getUsers(@RequestParam(required = false) String username) {
+        return ResponseEntity.ok(userService.findUsers(username));
     }
 }
--- src/main/java/org/example/schedule/service/UserService.java
+++ src/main/java/org/example/schedule/service/UserService.java
@@ -1,12 +1,16 @@
 package org.example.schedule.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.schedule.dto.UserGetAllResponse;
 import org.example.schedule.dto.UserSaveRequest;
 import org.example.schedule.dto.UserSaveResponse;
 import org.example.schedule.entity.User;
 import org.example.schedule.respository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +32,37 @@ public class UserService {
                 savedUser.getCreatedDate(),
                 savedUser.getModifiedDate()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserGetAllResponse> findUsers(String username) {
+        List<User> users = userRepository.findAll();
+        List<UserGetAllResponse> dtos = new ArrayList<>();
+
+        if (username == null) {
+            for (User user : users) {
+                dtos.add(new UserGetAllResponse(
+                        user.getId(),
+                        user.getUsername(),
+                        user.getEmail(),
+                        user.getCreatedDate(),
+                        user.getModifiedDate()
+                ));
+            }
+            return dtos;
+        }
+
+        for (User user : users) {
+            if (username.equals(user.getUsername())) {
+                dtos.add(new UserGetAllResponse(
+                        user.getId(),
+                        user.getUsername(),
+                        user.getEmail(),
+                        user.getCreatedDate(),
+                        user.getModifiedDate()
+                ));
+            }
+        }
+        return dtos;
     }
 }
