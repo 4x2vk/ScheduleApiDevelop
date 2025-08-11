6ac56765c5efaa59393fcb8183bbe63b97b3ce0a
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-11 18:57:49 +0900

    [feat] login filter config

--- src/main/java/org/example/schedule/common/filter/FilterConfig.java
+++ src/main/java/org/example/schedule/common/filter/FilterConfig.java
@@ -1,4 +1,17 @@
 package org.example.schedule.common.filter;
 
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter()); // Filter 등록
+        filterRegistrationBean.setOrder(1); // 필터는 체인으로 동작한다. 따라서 순서가 필요하다. 낮을 수록 먼저 동작한다.
+        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+        return filterRegistrationBean;
+    }
 }
