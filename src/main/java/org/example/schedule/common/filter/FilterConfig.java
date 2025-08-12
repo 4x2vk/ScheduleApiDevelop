039159f5db8c77c79a52f53fbe22fd947e67d4ee
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-12 15:09:50 +0900

    [feat] add @Configuration to FilterConfig

--- src/main/java/org/example/schedule/common/filter/FilterConfig.java
+++ src/main/java/org/example/schedule/common/filter/FilterConfig.java
@@ -3,7 +3,9 @@ package org.example.schedule.common.filter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FilterConfig {
 
     @Bean
