e3c8bcc5ec68cc31483d72eb95885f2c9ef97663
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-13 14:33:01 +0900

    [feat] validation for schedule requests

--- src/main/java/org/example/schedule/schedule/dto/ScheduleSaveRequestDto.java
+++ src/main/java/org/example/schedule/schedule/dto/ScheduleSaveRequestDto.java
@@ -1,11 +1,17 @@
 package org.example.schedule.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ScheduleSaveRequestDto {
 
+    @NotBlank(message = "title is required")
+    @Size(max = 5, message = "at least 5 symbols")
     private String title;
+
+    @NotBlank(message = "description is required")
+    @Size(max = 255, message = "max 255 symbols")
     private String description;
-    private Long userId;
 }
