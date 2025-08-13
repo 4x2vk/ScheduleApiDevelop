be7c010ad4d7c294d57045c3daa4312c68b67d61
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-13 14:23:42 +0900

    [feat] validation for user requests

--- src/main/java/org/example/schedule/user/dto/UserSaveRequest.java
+++ src/main/java/org/example/schedule/user/dto/UserSaveRequest.java
@@ -1,11 +1,18 @@
 package org.example.schedule.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserSaveRequest {
 
-    private String username;
+    @NotBlank(message = "email required")
+    @Email(message = "input valid email")
     private String email;
+
+    @NotBlank(message = "password required")
+    @Size(min = 6, message = "password at least 6 symbols")
     private String password;
 }
