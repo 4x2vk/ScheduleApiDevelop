7e02918ddbd1b0dccca7fa6971d3d1d1508bc91a
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-08-13 17:12:43 +0900

    [feat] securityFilterChain for http requests

--- src/main/java/org/example/schedule/common/config/SecurityConfig.java
+++ src/main/java/org/example/schedule/common/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package org.example.schedule.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +15,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Postman 테스트에 대한 CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/signup").permitAll() // 등록 허용
+                        .anyRequest().authenticated() // 나머지는 승인이 필요합니다
+                );
+        return http.build();
+    }
+
 }
